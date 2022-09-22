f2d4fc9fc18c3025436afb2f4ac967d32235b3d3
eloygplaza <smxegarcia@gmail.com>
2022-09-22 15:52:38 +0200

    heck Nan updated

--- index.js
+++ index.js
@@ -11,33 +11,37 @@ while (true) {
 
   const operator = prompt("Que operacion deseas realizar: ");
 
-  // we check if it is a number
-  if (operator.isInteger) {
-    // if is 1, we close the program
-    if (operator === "1") break;
+  // we check if it is a number or is the option selected is between the ones that we created
+  if (isNaN(operator || (operator > 5 && operator < 1))) {
+    console.log("Escribe una opcion correcta");
+    continue;
+  }
 
-    switch (operator) {
-      case "2":
-        let n1 = prompt("Escribe el primer numero: ");
-        let n2 = prompt("Escribe el segundo numero: ");
-        console.log("Tu resultado es: " + (parseInt(n1) + parseInt(n2)));
-        break;
-      case "3":
-        let n3 = prompt("Escribe el primer numero: ");
-        let n4 = prompt("Escribe el segundo numero: ");
-        console.log("Tu resultado es: " + (parseInt(n3) - parseInt(n4)));
-        break;
-      case "4":
-        let n5 = prompt("Escribe el primer numero: ");
-        let n6 = prompt("Escribe el segundo numero: ");
-        console.log("Tu resultado es: " + parseInt(n5) * parseInt(n6));
-        break;
-      case "5":
-        let n7 = prompt("Escribe el primer numero: ");
-        let n8 = prompt("Escribe el segundo numero: ");
-        console.log("Tu resultado es: " + parseInt(n7) / parseInt(n8));
-      default:
-        break;
-    }
+  // if is 1, we close the program
+  if (operator === "1") break;
+
+  // we check with option selected the user
+  switch (operator) {
+    case "2":
+      let n1 = prompt("Escribe el primer numero: ");
+      let n2 = prompt("Escribe el segundo numero: ");
+      console.log("Tu resultado es: " + (parseInt(n1) + parseInt(n2)));
+      break;
+    case "3":
+      let n3 = prompt("Escribe el primer numero: ");
+      let n4 = prompt("Escribe el segundo numero: ");
+      console.log("Tu resultado es: " + (parseInt(n3) - parseInt(n4)));
+      break;
+    case "4":
+      let n5 = prompt("Escribe el primer numero: ");
+      let n6 = prompt("Escribe el segundo numero: ");
+      console.log("Tu resultado es: " + parseInt(n5) * parseInt(n6));
+      break;
+    case "5":
+      let n7 = prompt("Escribe el primer numero: ");
+      let n8 = prompt("Escribe el segundo numero: ");
+      console.log("Tu resultado es: " + parseInt(n7) / parseInt(n8));
+    default:
+      break;
   }
 }
